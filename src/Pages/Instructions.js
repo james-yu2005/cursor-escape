355c944dd3dcd9954f7885ae357ba19826b64788
James Yu <james.yuu@hotmail.com>
2024-03-31 21:26:15 -0400

    chaged instructions and added a level and endpage

--- src/Pages/Instructions.js
+++ src/Pages/Instructions.js
@@ -4,8 +4,7 @@ function Instructions(){
     return(
         <>
             <h1 class="text-center mt-20 text-2xl">Instructions</h1>
-            <h2 class="text-center p-10 mr-20 ml-20">The goal of the game is to move your cursor to the green areas while avoiding the red areas. 
-               You cannot pass through black areas. You can change your cursor to different items with the drop down menu in the Ready Page. Enjoy the game!</h2>
+            <h2 class="text-center p-10 mr-20 ml-20">The goal of the game is to move your cursor to the green areas while avoiding the black areas that you cannot pass through. Enjoy the game!</h2>
             <div class="flex flex-col items-center justify-center">
                 <button onClick={() => navigate('/')} class="mt-4 p-2 bg-blue-400 rounded-lg hover:bg-purple-300 w-39">Homepage</button>
             </div>
